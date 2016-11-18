c783ed178f537a758399ba77acdb6b696ceecc79
adrianaVN <venegasadriana78@gmail.com>
2016-11-18 16:30:41 +0000

    portafolio

---
+++ js/main.js
@@ -0,0 +1,47 @@
+/*$(document).ready(function(){
+    $('.nube').mouseenter(function(){
+        $(this).fadeTo('fast','0.25');
+        });
+    /*$('.nube').mouseleave(function(){
+        $(this).fadeTo('fast', '1');
+      });
+});*/
+
+
+
+
+
+	function openNav() {
+	    document.getElementById("mySidenav").style.width = "250px";
+	}
+
+	function closeNav() {
+	    document.getElementById("mySidenav").style.width = "0";
+	}
+
+
+$(window).scroll(function() {
+
+    if ($(this).scrollTop()>100)
+     {
+        $('.olas').fadeOut();
+     }
+    else
+     {
+      $('.olas').fadeIn();
+     }
+ });
+
+ $(document).ready(function(){ 
+ 	$('.skillbar').each(function(){ 
+ 		jQuery(this).find('.skillbar-bar').animate({ width:jQuery(this).attr('data-percent') },6000);
+ 		 }); 
+
+ 	/*$(document).scroll(function(){
+
+ 		$('.olas').hide();
+
+ 		var scrollTop = $(this).scrollTop();
+
+ 	});*/
+ });
