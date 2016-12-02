de552938b4a71f917ca08fa27a1be12ea7b5c0f5
adrianaVN <venegasadriana78@gmail.com>
2016-12-02 19:01:59 +0000

    menu

--- js/main.js
+++ js/main.js
@@ -42,6 +42,22 @@ $(document).ready(function() {
     active2 = !active2;
     active3 = !active3;
     active4 = !active4;
-      
+
     });
+});
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 550
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
